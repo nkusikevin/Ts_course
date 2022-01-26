259f56819cc801e4fc3e42bf9793aaf3e19a85c9
nkusikevin <nkusikvnhart@gmail.com>
2022-01-26 19:34:24 +0200

    adding fns

--- basics/index.ts
+++ basics/index.ts
@@ -2,7 +2,22 @@ import axios from "axios"
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo{
+    id: number;
+    title: string;
+    completed: boolean;
+}
 axios.get(url).then(Response => {
-
-    console.log(Response.data)
+    const todo = Response.data as Todo;
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+   logTodo(id,title, completed);
 })
+
+
+const logTodo = (id:number,title:string,completed:boolean)=>{
+     console.log(`The Todo with ID:${id} 
+   Has a title: ${title} 
+   Is Completed ${completed}`);
+}
