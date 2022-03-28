3437d78320f53421ad832bc884dc9f040724b08e
nkusikevin <nkusikvnhart@gmail.com>
2022-03-28 22:14:09 +0200

    adding google map api

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,3 +1,10 @@
 import { User } from "./User";
 import {Company} from "./Company"
 
+
+
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: {lat: -34.397, lng: 150.644}
+});
+
