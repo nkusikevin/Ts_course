34ea59ae3bd64dc0dbbbba67e6207202e876e000
nkusikevin <nkusikvnhart@gmail.com>
2022-04-07 21:17:41 +0200

    adding only strings sorting algorithm

--- sort/build/index.js
+++ sort/build/index.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
+const charactersCollection = new CharactersCollection_1.CharactersCollection("Xaayb");
 const Numbers = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(Numbers);
+const sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(charactersCollection.data);
