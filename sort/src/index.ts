c4e93f47f01a7edf63ef4c28bdfd33004ab4f743
nkusikevin <nkusikvnhart@gmail.com>
2022-04-08 19:26:38 +0200

    linkedList sorting test

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,11 +1,21 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-const charactersCollection = new CharactersCollection("Xaayzhvhgvfxkngzafsghkmjoihbkkjhbfxab");
+const charactersCollection = new CharactersCollection(
+	"Xaayzhvhgvfxkngzafsghkmjoihbkkjhbfxab"
+);
 const Numbers = new NumbersCollection([10, 3, -5, 0]);
+const linkedList = new LinkedList();
 
-const sorter = new Sorter(charactersCollection);
+linkedList.add(400);
+linkedList.add(575);
+linkedList.add(66);
+const sorter = new Sorter(linkedList);
 sorter.sort();
+
+
+linkedList.print();
 
 console.log(charactersCollection.data);
